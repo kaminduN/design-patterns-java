312a727b3ccf07c705c5d5cc41a4708db33ba507
Kamindu <kamindun@users.noreply.github.com>
2024-05-03 22:37:04 +0530

    Update logs for holder pattern to get clear idea on execution path

--- singleton/src/main/java/com/kam/singleton/HolderSingleton.java
+++ singleton/src/main/java/com/kam/singleton/HolderSingleton.java
@@ -11,13 +11,29 @@ package com.kam.singleton;
 public class HolderSingleton {
 
     private HolderSingleton() {
+        super();
+        System.out.println("2. HolderSingleton created");
     }
 
+    // static inner class - inner classes are not loaded until they are
+    // referenced. This is a way to create a singleton class lazily.
     private static class SingletonHolder {
         private final static HolderSingleton SINGLETON_INSTANCE = new HolderSingleton();
     }
 
     public static HolderSingleton getInstance() {
+        System.out.println("1. HolderSingleton getInstance");
         return SingletonHolder.SINGLETON_INSTANCE;
     }
+
+
+//    public static void main(String[] args) {
+//        HolderSingleton obj = HolderSingleton.getInstance();
+//        HolderSingleton anotherObj = HolderSingleton.getInstance();
+//        if (obj == anotherObj) {
+//            System.out.println("3. obj and anotherObj are the same instance");
+//        } else {
+//            System.out.println("FAIL: obj and anotherObj are NOT the same instance");
+//        }
+//    }
 }
